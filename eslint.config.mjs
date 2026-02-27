c6154bfafbe9b04b2b51fb0970eff5ea3944cca0
Michael DeRazon <mderazon@gmail.com>
2026-02-27 17:48:10 +0000

    Bump to eslint 10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,23 @@
-// @ts-check
-import eslint from "@eslint/js";
+import tsparser from "@typescript-eslint/parser";
 import { defineConfig } from "eslint/config";
+import obsidianmd from "eslint-plugin-obsidianmd";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import obsidianmd from "eslint-plugin-obsidianmd";
 import globals from "globals";
 
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
+export default defineConfig([
+  ...tseslint.configs.recommendedTypeChecked,
   // @ts-expect-error - The types for this plugin don't perfectly match the expected FlatConfig format yet
-  ...obsidianmd.configs.recommendedWithLocalesEn,
+  ...(obsidianmd.configs?.recommendedWithLocalesEn || []),
   eslintPluginPrettierRecommended,
   {
+    files: ["**/*.ts"],
     languageOptions: {
       globals: {
         ...globals.browser,
+        ...globals.node,
       },
+      parser: tsparser,
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -26,4 +27,4 @@ export default defineConfig(
       "@typescript-eslint/no-deprecated": "error",
     },
   },
-);
+]);
